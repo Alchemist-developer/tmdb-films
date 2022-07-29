4f1cf34097474c00660c8a8d1625f3e513a71a77
Adryel Simon <adryel@live.it>
2022-07-29 15:26:47 -0300

    fix: home-page route

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const moviesURL = import.meta.env.VITE_API;
-const apiKey = import.meta.env.VITE_API_KEY;
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
 
-// import { Container } from './styles';
-
-function Home() {
-  console.log(moviesURL, apiKey + 'aqui');
-  // const [topMovies, setTopMovies] = useState([]);
-  
-  // const getTopRatedMovies = async (url) => {
-  //   const res = await fetch(url);
-  //   const data = await res.json();
-
-
-
-  // useEffect(() => {
-
-  //   const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
-  //   // console.log(topRatedURL);
-  //   getTopRatedMovies(topRatedURL);
-
-  // }, [])
-
-  // console.log(moviesURL, apiKey);
+const Home = () => {
 
   return (
-  <>Home</>
-  );
+    <div>Home haha</div>
+  )
 }
 
 export default Home;
