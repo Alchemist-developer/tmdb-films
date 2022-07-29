cba3f5117f89b416c31b5f48565d423ec1fb0c7e
Adryel Simon <adryel@live.it>
2022-07-29 19:42:37 -0300

    feat: search page | useSearchParams | hooks

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../../components/MovieCard";
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+
   return (
-    <div>Search</div>
-  )
-}
+    <div className="container mx-auto ">
+      <h1 className="ml-2 text-2xl hover:text-white  cursor-pointer py-5 w-max transition ease-in-out duration-300 ">
+        Resultados para: <span className="text-yellow-100">{query}</span>
+      </h1>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-5">
+        {movies.length === 0 && <p className="text-white">Carregando...</p>}
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
