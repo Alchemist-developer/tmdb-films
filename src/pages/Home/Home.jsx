49e7a533ad7b6c1de8ec70e0312f624f6bb356bc
Adryel Simon <adryel@live.it>
2022-07-29 03:34:58 -0300

    feat: init api fetch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { useState, useEffect } from 'react';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 // import { Container } from './styles';
 
 function Home() {
-  return <>Home</>;
+  console.log(moviesURL, apiKey + 'aqui');
+  // const [topMovies, setTopMovies] = useState([]);
+  
+  // const getTopRatedMovies = async (url) => {
+  //   const res = await fetch(url);
+  //   const data = await res.json();
+
+
+
+  // useEffect(() => {
+
+  //   const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
+  //   // console.log(topRatedURL);
+  //   getTopRatedMovies(topRatedURL);
+
+  // }, [])
+
+  // console.log(moviesURL, apiKey);
+
+  return (
+  <>Home</>
+  );
 }
 
 export default Home;
