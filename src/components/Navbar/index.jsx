33ce32a7ce6c7a006c9b89c28ae6d566da806ecb
Adryel Simon <adryel@live.it>
2022-07-29 19:43:15 -0300

    feat: navbar input search function

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 import "./index.css";
 
 function Navbar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if(!search) return;
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
+    
   return (
     <nav
       id="navbar"
@@ -14,11 +25,13 @@ function Navbar() {
           Alchemistic Movies
         </Link>
       </h2>
-      <form className="bg-gray-500 rounded px-5 ">
+      <form onSubmit={handleSearch} className="bg-gray-500 rounded px-5">
         <input
           type="text"
           placeholder="Busque um filme"
           className="text-start py-1 px-2 rounded-lg bg-gray-500"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
         />
         <button type="submit" className="ml-5 hover:bg-transparent">
           <BiSearchAlt2 className="transition ease-in-out duration-300 mt-2 text-black hover:text-red-600" />
