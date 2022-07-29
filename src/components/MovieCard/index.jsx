538544893f6bb65b67799069964b1ed82b5d67ec
Adryel Simon <adryel@live.it>
2022-07-29 18:01:08 -0300

    style: moviecard component img size&measurements

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -6,13 +6,13 @@ const imageUrl = import.meta.env.VITE_IMG;
 const MovieCard = ({ movie, showLink = true }) => {
   return (
     <div className="">
-      <img src={imageUrl + movie.poster_path} alt={movie.title} />
-      <h2>{movie.title}</h2>
-      <p>
-        <FaStar /> {movie.vote_average}
+      <img className="h-80" src={imageUrl + movie.poster_path} alt={movie.title} />
+      <h2 className="py-1">{movie.title}</h2>
+      <p className="flex flex-row items-center text-xs mb-1">
+        <FaStar className="mr-2 text-yellow-500"/> {movie.vote_average}
       </p>
       {showLink && 
-      <Link to={`/movie/${movie.id}`}>
+      <Link className="text-black rounded opacity-50 p-1 px-3 bg-white hover:ml-1 hover:text-gray-500" to={`/movie/${movie.id}`}>
       Detalhes
       </Link>}
     </div>
