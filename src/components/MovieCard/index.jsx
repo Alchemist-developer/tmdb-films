d89d138d1af434b22d61bf3a6694bfbe14906dc2
Adryel Simon <adryel@live.it>
2022-07-29 23:40:46 -0300

    style: flexbox&position

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -5,9 +5,9 @@ const imageUrl = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
   return (
-    <div className="bg-gray-900 mx-auto p-3 px-5 rounded">
+    <div className="flex flex-col bg-gray-900 mx-auto p-3 px-5 rounded">
       <img
-        className="py-1 h-80 max-w-full"
+        className="rounded-md py-1 h-80 max-w-full"
         src={imageUrl + movie.poster_path}
         alt={movie.title}
       />
@@ -16,7 +16,7 @@ const MovieCard = ({ movie, showLink = true }) => {
         <FaStar className="mr-2 text-yellow-500" /> {movie.vote_average}
       </p>
       {showLink && (
-        <div className="flex justify-center">
+        <div className="flex justify-center items-end">
           <Link
             className="transition ease-in-out duration-500 text-center w-full text-black rounded opacity-50 p-1 border-2 bg-white hover:bg-transparent hover:border-2 hover:border-yellow-100 hover:text-yellow-500"
             to={`/movie/${movie.id}`}
