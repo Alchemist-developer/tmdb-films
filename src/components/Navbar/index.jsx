73737ead85c9f916e0a0a16fe99d59e211efa780
Adryel Simon <adryel@live.it>
2022-07-29 23:41:24 -0300

    style: position&hover&mobile

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -17,15 +17,15 @@ function Navbar() {
   return (
     <nav
       id="navbar"
-      className="flex flex-row justify-between bg-black py-5 px-10 rounded items-center"
+      className="shadow-[0_1px_33px_-30px_rgba(255,255,255)] flex flex-wrap justify-between bg-black py-5 px-10 items-center"
     >
       <h2>
-        <Link to="/" className="text-white flex flex-row text-3xl">
-          <BiCameraMovie className="mr-3 mt-1 " />
-          Alchemistic Movies
+        <Link to="/" className="text-white flex flex-row font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-black to-yellow-100">
+          <BiCameraMovie className="mr-3 mt-1" />
+          ALCHEMISTIC MOVIES
         </Link>
       </h2>
-      <form onSubmit={handleSearch} className="bg-gray-500 rounded px-5">
+      <form onSubmit={handleSearch} className="my-4 bg-gray-500 rounded px-5">
         <input
           type="text"
           placeholder="Busque um filme"
@@ -34,7 +34,7 @@ function Navbar() {
           value={search}
         />
         <button type="submit" className="ml-5 hover:bg-transparent">
-          <BiSearchAlt2 className="transition ease-in-out duration-300 mt-2 text-black hover:text-red-600" />
+          <p className="transition ease-in-out duration-300 hover:sepia">⚗️</p>
         </button>
       </form>
     </nav>
